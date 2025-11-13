cfa1571c04e255bff55c8003632b9cc3e44b6f7d
Kainuru Balaji <kainurubalaji@gmail.com>
2025-11-13 09:00:00 +0530

    Create Notification model for all user types

---
+++ models/Notification.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose")
+
+const notificationSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["info", "success", "warning", "error", "booking", "order", "payment", "delivery"],
+    default: "info",
+  },
+  isRead: {
+    type: Boolean,
+    default: false,
+  },
+  link: {
+    type: String,
+  },
+  relatedId: {
+    type: mongoose.Schema.Types.ObjectId,
+  },
+  relatedModel: {
+    type: String,
+    enum: ["Booking", "Order", "Payment", "User"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+notificationSchema.index({ user: 1, createdAt: -1 })
+notificationSchema.index({ user: 1, isRead: 1 })
+
+module.exports = mongoose.model("Notification", notificationSchema)
